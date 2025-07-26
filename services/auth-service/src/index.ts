fad0af2885f63721b7a77932a77175bc2174c8a0
Alysstair Morales <al@tulaytech.co>
2025-07-26 23:31:40 +0800

    added openauth

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,18 +1,37 @@
-import { Hono } from 'hono'
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { handle } from "hono/aws-lambda";
+import { issuer } from "@openauthjs/openauth";
+import { CodeUI } from "@openauthjs/openauth/ui/code";
+import { CodeProvider } from "@openauthjs/openauth/provider/code";
+import { MemoryStorage } from "@openauthjs/openauth/storage/memory";
+import { subjects } from "./subjects";
 
-const app = new Hono()
+async function getUser(email: string) {
+  // Get user from database and return user ID
+  return "123";
+}
 
+const app = issuer({
+  subjects,
+  storage: MemoryStorage(),
+  // Remove after setting custom domain
+  allow: async () => true,
+  providers: {
+    code: CodeProvider(
+      CodeUI({
+        sendCode: async (email, code) => {
+          console.log(email, code);
+        },
+      })
+    ),
+  },
+  success: async (ctx, value) => {
+    if (value.provider === "code") {
+      return ctx.subject("user", {
+        id: await getUser(value.claims.email),
+      });
+    }
+    throw new Error("Invalid provider");
+  },
+});
 
-const db = drizzle(process.env.DATABASE_URL!);
-
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default { 
-  port: 3001, 
-  fetch: app.fetch, 
-} 
+export const handler = handle(app);
