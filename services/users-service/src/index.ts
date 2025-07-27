1b81e6fc5a0bac47a7c44efc482625c5e81d9936
Alysstair Morales <al@tulaytech.co>
2025-07-27 20:02:39 +0800

    connected users db to users service

--- services/users-service/src/index.ts
+++ services/users-service/src/index.ts
@@ -1,18 +1,16 @@
-import { Hono } from 'hono'
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { Hono } from "hono";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-const app = new Hono()
-
+const app = new Hono();
 
 const db = drizzle(process.env.DATABASE_URL!);
 
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default { 
-  port: 3008, 
-  fetch: app.fetch, 
-} 
+export default {
+  port: 3008,
+  fetch: app.fetch,
+};
